1f3f3e63fd2bdbbfc88ebb4043f751f4f9dbd2a4
Euwan Abogadie <yoowan28@gmail.com>
2026-05-04 09:10:12 +0800

    3

--- start.bat
+++ start.bat
@@ -1,5 +1,10 @@
 @echo off
 cd /d "C:\Coding Projects\App Dev\Smart Nutri Scanner App\smartnutri"
-start cmd /k "npx expo start --web"
+echo Installing dependencies...
+npm install firebase
+npm install @react-navigation/native @react-navigation/bottom-tabs @react-navigation/native-stack
+npx expo install react-native-screens react-native-safe-area-context
+echo Starting Expo...
+npx expo start
 timeout /t 5
 start http://localhost:8081
